75ddeec509d56e3ccc9fd698af290f41a0c0b8e2
omkar342 <omkarjadhav095@gmail.com>
2024-04-15 14:32:46 +0530

    Initializing the app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//App.js 
+import React from "react"; 
+import NutritionMeter from "./NutritionMeter"; 
+
+function App() { 
+return ( 
+	<div className="bg-gray-100 min-h-screen"> 
+	<NutritionMeter /> 
+	</div> 
+); 
+} 
+
+export default App; 
